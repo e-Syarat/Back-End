84b31a6d57f1993e4328c082738cd3cc147cd145
ZENI RAMADAN <zeni10222017@sttcipasung.ac.id>
2025-06-08 21:47:06 +0700

    perbaiki format kode, konsisten menggunakan tanda kutip ganda, dan menambahkan pengaturan header CORS pada rute statis untuk '/number'

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const dotenv = require('dotenv');
-const router = require('./routes/index.js');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const router = require("./routes/index.js");
 dotenv.config();
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use('/alfabet', express.static(path.join(__dirname, './public/alfabet'), {
-  setHeaders: (res, path) => {
-    res.set('Cross-Origin-Resource-Policy', 'cross-origin');
-  }
-}));
-app.use('/number', express.static(path.join(__dirname, './public/number'), {
+app.use(
+  "/alfabet",
+  express.static(path.join(__dirname, "./public/alfabet"), {
     setHeaders: (res, path) => {
-      res.set('Cross-Origin-Resource-Policy', 'cross-origin');
-    }
-}));
+      res.set("Cross-Origin-Resource-Policy", "cross-origin");
+    },
+  })
+);
+app.use(
+  "/number",
+  express.static(path.join(__dirname, "./public/number"), {
+    setHeaders: (res, path) => {
+      res.set("Cross-Origin-Resource-Policy", "cross-origin");
+    },
+  })
+);
 //routes
-app.use('/api', router);
+app.use("/api", router);
 
 module.exports = app;
